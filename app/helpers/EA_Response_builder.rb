08b38102eff2f4c773a66dfa6d5d495b093faca9
venu211 <venumadhav.ror@gmail.com>
2017-09-26 14:40:52 -0400

    calling only HDI q3j all times and ignoring enroll_app_system_wrapper for life event request from Nitish

--- app/helpers/EA_Response_builder.rb
+++ app/helpers/EA_Response_builder.rb
@@ -19,27 +19,33 @@ extend Publish
         #{"applId":"xxxxxxx"}
         acrn_payload = {"appRefNum" => curam_res.xpath("//AppCaseRef").text}.to_s.gsub("=>", ":")
         ic_payload =   {"icNumber" => curam_res.xpath("//integrated_case_reference").text}.to_s.gsub("=>", ":")
-        begin
-        puts "calling: enroll_app_system_wrapper"
-        haven_response = RestClient.post('newsafehaven.dcmic.org/enroll_app_system_wrapper.php', acrn_payload, {content_type: :"application/json", accept: :"application/json"})
-        $CURAM_LOG.info("#{haven_response}")
+          puts "calling :  ic_system_wrapper_q3j"
+          hdi_call = RestClient.post('newsafehaven.dcmic.org/ic_system_wrapper_q3j.php', ic_payload, {content_type: :"application/json", accept: :"application/json"})
+          $CURAM_LOG.info("Arbitrage triggered ic_system_wrapper_q3j.php webservice. Response: #{hdi_call.body}")
+          $CURAM_LOG.info("#{ic_payload}")
+      
+      # Temporarily commented the begin to end code for life events change from nitish
+        # begin
+        # puts "calling: enroll_app_system_wrapper"
+        # haven_response = RestClient.post('newsafehaven.dcmic.org/enroll_app_system_wrapper.php', acrn_payload, {content_type: :"application/json", accept: :"application/json"})
+        # $CURAM_LOG.info("#{haven_response}")
         
        
 
-         if JSON.parse(haven_response.body).to_h.keys.include?("ACRN Response") 
-          puts "calling :  ic_system_wrapper_q3j"
-          no_acrn = RestClient.post('newsafehaven.dcmic.org/ic_system_wrapper_q3j.php', ic_payload, {content_type: :"application/json", accept: :"application/json"})
-          $CURAM_LOG.info("ACRN not found and Arbitrage triggered ic_system_wrapper_q3j.php webservice. Response: #{no_acrn.body}")
-          $CURAM_LOG.info("#{ic_payload}")
-        end
-        rescue 
-          payload = "Error: Unable to get JSON evaluation data set from Haven by calling 
-            enroll_app_system_wrapper.php webservice or ic_system_wrapper_q3j.php using Apprefnum: #{ic_payload}"
-          email_notice(payload)
-          #RestClient.post('newsafehaven.dcmic.org/external_log.php', payload)
-          $CURAM_LOG.info("#{payload}")
+        #  if JSON.parse(haven_response.body).to_h.keys.include?("ACRN Response") 
+        #   puts "calling :  ic_system_wrapper_q3j"
+        #   no_acrn = RestClient.post('newsafehaven.dcmic.org/ic_system_wrapper_q3j.php', ic_payload, {content_type: :"application/json", accept: :"application/json"})
+        #   $CURAM_LOG.info("ACRN not found and Arbitrage triggered ic_system_wrapper_q3j.php webservice. Response: #{no_acrn.body}")
+        #   $CURAM_LOG.info("#{ic_payload}")
+        # end
+        # rescue 
+        #   payload = "Error: Unable to get JSON evaluation data set from Haven by calling 
+        #     enroll_app_system_wrapper.php webservice or ic_system_wrapper_q3j.php using Apprefnum: #{ic_payload}"
+        #   email_notice(payload)
+        #   #RestClient.post('newsafehaven.dcmic.org/external_log.php', payload)
+        #   $CURAM_LOG.info("#{payload}")
 
-        end
+        # end
 
     end #method
 
